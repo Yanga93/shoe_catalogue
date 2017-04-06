2934f413197380c7e838d545a1e832f19da0bacd
Yanga93 <yangazukelwa@gmail.com>
2017-04-06 16:57:01 +0200

    new updates for filter the dropdown

--- shoe_catalogue.js
+++ shoe_catalogue.js
@@ -20,16 +20,34 @@ var shoes = [{
 
   },
   {
+    color: 'Black',
+    price: '850.00',
+    in_stock: '5',
+    size: '7',
+  },
+  {
     color: 'Brown',
-    price: '650.00',
+    price: '900.00',
     in_stock: '3',
-    size: '6',
+    size: '7',
+  },
+  {
+    color: 'Brown',
+    price: '850.00',
+    in_stock: '5',
+    size: '8',
   },
   {
     color: 'White',
     price: '550.00',
     in_stock: '3',
-    size: '6',
+    size: '7',
+  },
+  {
+    color: 'White',
+    price: '750.00',
+    in_stock: '5',
+    size: '8',
   }
 ];
 
@@ -48,87 +66,60 @@ document.querySelector(".searchBtn").addEventListener("click", function() {
   //filter the data
   if (currentShoeColor === "All" && currentShoeSize === "All") {
     shoeArray = shoes;
+  } else {
+    for (var i = 0; i < shoes.length; i++) {
+      var shoe = shoes[i];
+      var colorMatches = (currentShoeColor === "All" || currentShoeColor === shoe.color);
+      var sizeMatches = (currentShoeSize === "All" || currentShoeSize === shoe.size);
+      if (colorMatches && sizeMatches) {
+        shoeArray.push(shoe);
+      }
+    }
   }
-  if(currentShoeColor !== "All" && currentShoeSize === "All") {
-    for (var i = 0; i < shoes.length; i++) {
-      var shoe = shoes[i];
-      if (shoe.color === currentShoeColor) {
-        shoeArray.push(shoe);
-      }
-    }
-  };
-  
-  if (currentShoeColor === "All" && currentShoeSize !== "All") {
-    for (var i = 0; i < shoes.length; i++) {
-      var shoe = shoes[i];
-      if (shoe.size === currentShoeSize) {
-        shoeArray.push(shoe);
-      }
-    }
-  };
-
-  //display the filtered data
+  // output.innerHTML
   document.querySelector(".tableOutcome").innerHTML = combineTemp({
     shoes: shoeArray
   });
 
 });
 
-// Add new item function starts here
-buttonAdd.addEventListener('click', function(){
-  var newStock = shoes.push({color: document.querySelector(".textshoeColor").value,
-  price: document.querySelector(".textshoePrice").value,
-  in_stock: document.querySelector(".textshoeQuantity").value,
-  size: document.querySelector(".textshoeSize").value
-});
-document.querySelector(".textshoeColor").value=""
-document.querySelector(".textshoePrice").value=""
-document.querySelector(".textshoeQuantity").value=""
-document.querySelector(".textshoeSize").value=""
-});
-
-
-
-
-
-// {
-//   color: 'Black',
-//   price: '450.00',
-//   in_stock: '5',
-//   size: '7',
-//
-//   },
-//   // ==== shoe size 7 and price 450 ===//
-//   {
-//     color: 'Brown',
-//     price: '750.00',
-//     in_stock: '3',
-//     size: '7',
-//   },
-//   {
-//     color: 'White',
-//     price: '850.00',
-//     in_stock: '3',
-//     size: '7',
-//   },
-//
-// // ==== shoe size 8 and price 550 ===//
-//   {
-//     color: 'Black',
-//     price: '550.00',
-//     in_stock: '4',
-//     size: '8',
-//
-//   },
-//   {
-//     color: 'Brown',
-//     price: '650.00',
-//     in_stock: '5',
-//     size: '8',
-//   },
-//   {
-//     color: 'White',
-//     price: '600.00',
-//     in_stock: '6',
-//     size: '8',
+// if (currentShoeColor === "All" && currentShoeSize === "All") {
+//   shoeArray = shoes;
+// else if (currentShoeColor !== "All" && currentShoeSize === "All") {
+//   for (var i = 0; i < shoes.length; i++) {
+//     var shoe = shoes[i];
+//     else(shoe.color === currentShoeColor) {
+//       shoeArray.push(shoe);
+//     }
 //   }
+// };
+// //
+// // if (currentShoeColor === "All" && currentShoeSize !== "All") {
+// //   for (var i = 0; i < shoes.length; i++) {
+// //     var shoe = shoes[i];
+// //     if (shoe.size === currentShoeSize) {
+// //       shoeArray.push(shoe);
+// //     }
+// //   }
+// // };
+//
+// //display the filtered data
+// document.querySelector(".tableOutcome").innerHTML = combineTemp({
+// shoes: shoeArray
+// });
+//
+// });
+
+// Add new item function starts here
+buttonAdd.addEventListener('click', function() {
+  var newStock = shoes.push({
+    color: document.querySelector(".textshoeColor").value,
+    price: document.querySelector(".textshoePrice").value,
+    in_stock: document.querySelector(".textshoeQuantity").value,
+    size: document.querySelector(".textshoeSize").value
+  });
+  document.querySelector(".textshoeColor").value = ""
+  document.querySelector(".textshoePrice").value = ""
+  document.querySelector(".textshoeQuantity").value = ""
+  document.querySelector(".textshoeSize").value = ""
+});
